48bbca2bda831a0b10ddf334305279c728783629
tanviba0404 <tanvibazala55@gmail.com>
2026-07-09 15:08:49 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,15 @@
+// Write a java program for introduction.
+
+public class intro
+{
+	public static void main(String args[])
+	{
+		
+		System.out.println("My name is Tanviba Zala");
+		System.out.println("I am currently purusing my BCA.");
+		System.out.println("I have strong interest in programming.");
+		System.out.println("My short term goal is to strengthen my programming language.");
+		System.out.println("Apart from academics,I enjoy listening to music.");
+		
+	}
+}	
