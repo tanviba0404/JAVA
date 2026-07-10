5e6a4b64973059c5188ab4ba536cc150e11b1c5e
tanviba0404 <tanvibazala55@gmail.com>
2026-07-10 16:06:01 +0530

    Add files via upload

---
+++ U1_P6_9.java
@@ -0,0 +1,24 @@
+//6.9 Write a java program to get name from user and ask how many times user want to print that name do..while loop.
+
+import java.util.Scanner;
+
+ public class U1_P6_9
+	   {
+	           public static void main(String args[])
+			   {
+			           Scanner scn = new Scanner(System.in);
+					   
+					   System.out.println("Enter name:");
+					   String name = scn.nextLine();
+					    
+					
+					   int i=1;
+					   do
+					   {
+						   System.out.println(name);
+						   i++;
+					   }while(i<=n)
+					  
+					scn.close();
+			   }
+	   }
