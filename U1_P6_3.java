cabd6972c1d8613dc58869854b4868d9bb08c523
tanviba0404 <tanvibazala55@gmail.com>
2026-07-10 16:03:16 +0530

    Add files via upload

---
+++ U1_P6_3.java
@@ -0,0 +1,23 @@
+//6.1 Write a java program to get different values from user and print 10 times using do..while loop.
+
+import java.util.Scanner;
+
+ public class U1_P6_3
+	   {
+	           public static void main(String args[])
+			   {
+			           Scanner scn = new Scanner(System.in);
+					   
+					   System.out.println("Enter values:");
+					   int value = scn.nextInt();
+					   
+					    int i=1;
+					 do
+					   {
+						   System.out.println("Value :" + i);
+						   i++;
+					   }while(i<=10);
+					  
+					scn.close();
+			   }
+	   }
