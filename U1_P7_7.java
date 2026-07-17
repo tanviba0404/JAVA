4605e29a78cf1dd9b34b7a9eebf3278ab6a58dc3
tanviba0404 <tanvibazala55@gmail.com>
2026-07-17 12:21:09 +0530

    Add files via upload

---
+++ U1_P7_7.java
@@ -0,0 +1,62 @@
+//7.7 Write a java program to use switch case condition.
+   
+   import java.util.Scanner;
+   
+   public class U1_P7_7
+   {
+	   public static void main(String args[])
+	   {
+		   Scanner scn = new Scanner(System.in);
+		   
+		   System.out.println("Enter month number(1-12):");
+		   int month = scn.nextInt();
+		   
+		   switch(month)
+		   {
+			   case 1:
+			   System.out.println("January");
+			   break;
+			   case 2:
+			   System.out.println("February");
+			   break;
+			   case 3:
+			   System.out.println("March");
+			   break;
+			   case 4:
+			   System.out.println("April");
+			   break;
+			   case 5:
+			   System.out.println("May");
+			   break;
+			   case 6:
+			   System.out.println("June");
+			   break;
+			   case 7:
+			   System.out.println("July");
+			   break;
+			   case 8:
+			   System.out.println("August");
+			   break;
+			   case 9:
+			   System.out.println("September");
+			   break;
+			   case 10:
+			   System.out.println("October");
+			   break;
+			   case 11:
+			   System.out.println("November");
+			   break;
+			   case 12:
+			   System.out.println("December");
+			   break;
+			   
+			   default:
+			   System.out.println("Invalid month number");
+		   }
+             scn.close();
+	   }
+   }
+   
+			   
+			   
+			   
