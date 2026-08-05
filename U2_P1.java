c5982111877152323cdc4c5fb2d336bbb05fbd99
tanviba0404 <tanvibazala55@gmail.com>
2026-08-05 15:48:48 +0530

    Add files via upload

---
+++ U2_P1.java
@@ -0,0 +1,21 @@
+class Animal{
+    String name = "Animal";
+    void eat (){
+          System.out.println("This animal eats food.");
+    }
+  
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("The dog barks:Woof!Woof!");
+    }
+}
+
+public class U2_P1{
+    public static void main(String args[]){
+        Dog mydog = new Dog();
+        mydog.eat();
+        mydog.bark();
+    }
+}
