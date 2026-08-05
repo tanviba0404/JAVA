d87f1e8ed226e76cb94e7c5e1bf43a4dfd74f2ae
tanviba0404 <tanvibazala55@gmail.com>
2026-08-05 15:49:23 +0530

    Add files via upload

---
+++ Unit 2/U2_P2.java
@@ -0,0 +1,28 @@
+class Animal{
+    String name = "Animal";
+    void eat (){
+          System.out.println("This animal eats food.");
+    }
+  
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("The dog barks:Woof!Woof!");
+    }
+}
+
+class Puppy extends Dog{
+    void weep(){
+        System.out.println("Puppy is weeping.");
+    }
+}
+
+public class U2_P2{
+    public static void main(String args[]){
+        Puppy p = new Puppy();
+        p.eat();
+        p.bark();
+        p.weep();
+    }
+}
