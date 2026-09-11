793201076b69148ad705280b2a46a79fac0781fb
tanviba0404 <tanvibazala55@gmail.com>
2026-09-11 15:18:06 +0530

    Add files via upload

---
+++ Unit 2/U2_P16.java
@@ -0,0 +1,9 @@
+import school.students.student;
+ public class U2_P16
+ {
+    public static void main(String[] args) 
+    {
+        student s = new student();
+        s.displayInfo();
+    }
+ }
---
+++ Unit 2/student.java
@@ -0,0 +1,11 @@
+package  school.students;
+
+public class student
+{
+    public void displayInfo()
+    {
+        String s1 = "Tanvi";
+        String s2 = "Rudri";
+        System.out.println("Class Toppers:" + s1+ "and"+ s2);
+    }
+}
