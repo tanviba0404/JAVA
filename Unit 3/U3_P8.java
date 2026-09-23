da539688748e27aa234c37d34125982050e19e0d
tanviba0404 <tanvibazala55@gmail.com>
2026-09-23 15:56:35 +0530

    Add files via upload

---
+++ Unit 3/U3_P8.java
@@ -0,0 +1,31 @@
+//Exception pro
+
+public class U3_P8
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            method1();
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Exception caught in main: " + e);
+        }
+    }
+
+    public static void method1()
+    {
+        method2();
+    }
+
+    public static void method2()
+    {
+        method3();
+    }
+
+    public static void method3()
+    {
+        int result=10/0;
+    }
+}
